1e0e73a908c617c1219334503e193aef4e3d7958
Kyou13 <kyou1013@gmail.com>
2017-11-01 17:41:06 +0900

    update testy.py

--- testy.py
+++ testy.py
@@ -22,14 +22,17 @@ import os
 import shutil
 import json
 import collections as cl
+import re
+import sys
 
 def main():
-
+    
+    args = sys.argv
     dirlist = os.listdir()
     ys = cl.OrderedDict()
     #for i in [m for m in dirlist if m.startswith('MOT')]:
     #for fileDir in ["MOT17-02-FRCNN"]:
-    fileDir = "MOT17-02-FRCNN"
+    fileDir = args[1]
     with open(fileDir + '/seqinfo.ini') as info:
         seqLen = int(info.readlines()[4].rstrip().split('=')[1])
         print("seqLength:{}".format(seqLen))
@@ -57,15 +60,24 @@ def main():
             filename = '{0:06d}'.format(i) + '.jpg'
             print(filename)
             data = cl.OrderedDict()
-            data["image_path"] = filename
-            print(rects)
+            data["image_path"] = '~/ml/MOT17/train/'+fileDir+'/'+filename
+            #print(rects)
             data["rect"] = rects
             ys[i] = data
         f.close()
-    fw = open('test.json','w')
+    fw = open(fileDir+'.json','w')
     json.dump(ys,fw,indent=4)
     fw.close()
+    fw = open(fileDir+'.json')
+    fo = open(fileDir+'_fin.json','w')
+
+    for conv_line in fw:
+        conv_line = re.sub(r'"[0-9]+": ',"",conv_line)
+        fo.write(conv_line)
+    fw.close()
+    fo.close()
     
+
     print("finish!!")
 
 if __name__=='__main__':
